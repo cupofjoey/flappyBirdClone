2fb98aea906486a74fd5e118e4a6ee2cadb26762
Joe Hill <joe@jkh3.com>
2017-09-04 19:23:19 -0400

    Created a Clone of the famous game Flappy Bird

---
+++ flappyBird/GameScene.swift
@@ -0,0 +1,129 @@
+//
+//  GameScene.swift
+//  flappyBird
+//
+//  Created by BishopHill on 9/4/17.
+//  Copyright © 2017 Dumb Unicorn. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var bird = SKSpriteNode()
+    
+    var bg = SKSpriteNode()
+    
+    func makePipes() {
+        
+        let movePipes = SKAction.move(by: CGVector(dx: -2 * self.frame.width, dy:0), duration: TimeInterval(self.frame.width / 100))
+        
+        let gapHeight = bird.size.height * 4.75
+        
+        let movementAmount = arc4random()  % UInt32(self.frame.height / 2)
+        
+        let pipeOffset = CGFloat(movementAmount) - self.frame.height / 4
+        
+        let pipeTexture = SKTexture(imageNamed: "pipe1.png")
+        
+        let pipe1 = SKSpriteNode(texture: pipeTexture)
+        
+        pipe1.position = CGPoint(x: self.frame.midX + self.frame.width, y: self.frame.midY + pipeTexture.size().height / 2 + gapHeight / 2 + pipeOffset)
+        
+        pipe1.run(movePipes)
+        
+        self.addChild(pipe1)
+        
+        let pipeTexture2 = SKTexture(imageNamed: "pipe2.png")
+        
+        let pipe2 = SKSpriteNode(texture: pipeTexture2)
+        
+        pipe2.position = CGPoint(x:self.frame.midX + self.frame.width, y: self.frame.midY - pipeTexture2.size().height / 2 - gapHeight / 2 + pipeOffset)
+        
+        pipe2.run(movePipes)
+        
+        self.addChild(pipe2)
+        
+    }
+    
+    
+    override func didMove(to view: SKView) {
+        
+        _ = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.makePipes), userInfo: nil, repeats: true)
+        
+        let bgTexture = SKTexture(imageNamed: "bg.png")
+        
+        let moveBGAnimation = SKAction.move(by: CGVector(dx: -bgTexture.size().width, dy: 0), duration: 7)
+        let shiftBGAnimation = SKAction.move(by: CGVector(dx: bgTexture.size().width, dy: 0), duration: 0)
+        let moveBGForever = SKAction.repeatForever(SKAction.sequence([moveBGAnimation, shiftBGAnimation]))
+        
+        var i: CGFloat = 0
+        
+        while i < 3 {
+            
+            bg = SKSpriteNode(texture: bgTexture)
+            
+            bg.position = CGPoint(x: bgTexture.size().width * i, y: self.frame.midY)
+            
+            bg.size.height = self.frame.height
+            
+            bg.run(moveBGForever)
+            
+            bg.zPosition = -1
+            
+            self.addChild(bg)
+            
+            i += 1
+            
+        }
+        
+        
+        let birdTexture = SKTexture(imageNamed: "flappy1.png")
+        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
+        
+        let animation = SKAction.animate(with: [birdTexture, birdTexture2], timePerFrame: 0.1)
+        let makeBirdFlap = SKAction.repeatForever(animation)
+        
+        bird = SKSpriteNode(texture: birdTexture)
+        
+        
+        bird.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        
+        bird.run(makeBirdFlap)
+        
+        self.addChild(bird)
+        
+        let ground = SKNode()
+        
+        ground.position = CGPoint(x: self.frame.midX, y: -self.frame.height / 2)
+        
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.width, height: 1))
+        
+        ground.physicsBody!.isDynamic = false
+        
+        self.addChild(ground)
+        
+        
+    }
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        let birdTexture = SKTexture(imageNamed: "flappy1.png")
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height / 2)
+        
+        bird.physicsBody!.isDynamic = true
+        bird.physicsBody!.velocity = CGVector(dx: 0, dy: 0)
+        bird.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 80))
+        
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+
+
+}
